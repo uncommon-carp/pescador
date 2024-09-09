f2d7f9be2db9ca25f7421ae42144c88501a42d19
Cory Sorel <csorel.creative@gmail.com>
2024-09-09 15:32:32 -0500

    interfaces

--- packages/interfaces/src/conditions/service.ts
+++ packages/interfaces/src/conditions/service.ts
@@ -1,8 +1,22 @@
-import { CurrentWeather } from "@pescador/graph";
+import { BulkStation, CurrentWeather } from "@pescador/graph";
 
 export interface GetWeatherByZipFunction {
   serviceName: "service-conditions";
   functionName: "getWeatherByZip";
   input: { zip: string };
   output: CurrentWeather;
+}
+
+export interface GetStationsByBoxFunction {
+  serviceName: "service-conditions";
+  functionName: "getStationsByBox";
+  input: { lat: number; lng: number };
+  output: BulkStation;
+}
+
+export interface GetStationsById {
+  serviceName: "service-conditions";
+  functionName: "getStationsById";
+  input: { ids: string[] };
+  output: BulkStation;
 }
