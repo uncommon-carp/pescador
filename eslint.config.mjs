aed4e72cfba40049f96eec76d025d84d894be62b
Cory Sorel <csorel.creative@gmail.com>
2025-07-02 22:31:56 -0500

    linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     rules: {
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
     },
   },
 );
